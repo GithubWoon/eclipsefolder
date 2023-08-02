6ccce9a09e4b74d7ed79005e5b0ced81979a795d
GithubWoon <eo2458@naver.com>
2023-08-02 17:51:16 +0900

    8월2일

--- github/src/VariableParctice4/실습문제4.java
+++ github/src/VariableParctice4/실습문제4.java
@@ -1,9 +1,17 @@
 package VariableParctice4;
 
+import java.util.Scanner;
+
 public class 실습문제4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int people = sc.nextInt();
+		int candy = sc.nextInt();
+		float A = // 몫
+		float B = // 나머지
 
 	}
 
