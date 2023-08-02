301e6a55bf50e3ca66209b17613328aa7c99622e
GithubWoon <eo2458@naver.com>
2023-08-02 12:49:02 +0900

    깃깃깃깃

--- github/src/arcon/study4.java
+++ github/src/arcon/study4.java
@@ -4,7 +4,21 @@ public class study4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// 키보드 입력 객체
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int iCondition = 0;
+			System.out.println("메뉴");
+			System.out.println("우승조건");
+			System.out.println("우승조건2");
+			System.out.println("종료");
+			System.out.println("우승조건 선택");
+			
+		}
 
 	}
 
 }
+
+// random
