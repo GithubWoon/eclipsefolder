e678d98f159e90d33dfbd5d7f0576e52bd13b41b
GithubWoon <eo2458@naver.com>
2023-08-04 10:20:53 +0900

    8월 4일

---
+++ day0805/src/arcon/버블정렬알고리즘.java
@@ -0,0 +1,19 @@
+package arcon;
+
+public class 버블정렬알고리즘 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] scores = {60, 50, 95, 80, 70};
+		sor t (scores);
+		for (int i = 0;) i < scores.length ; i++ {
+			System.out.print(scores[i] + " ");
+		}
+
+	}
+	public static void sor(int [] arraus) {
+		for (int i = 0; i < arrays.length - i - 1; j++) {
+			if (arrays[j]) >
+		}
+	}
+}
