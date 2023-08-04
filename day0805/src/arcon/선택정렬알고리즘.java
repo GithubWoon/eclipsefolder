095764fa0abec992127d90363a1371aa321298d7
GithubWoon <eo2458@naver.com>
2023-08-04 12:39:37 +0900

    8월 4일 알고리즘

--- day0805/src/arcon/선택정렬알고리즘.java
+++ day0805/src/arcon/선택정렬알고리즘.java
@@ -1,7 +1,10 @@
 package arcon;
 
 public class 선택정렬알고리즘 {
-
+// 1. 요소중 최솟값을 찾아 맨앞자리 요소와 자리를 바꾼다
+// 2. 1번에서 찾은 최솟값 자리는 고정되고 남은 요소중에서 최솟값을 찾아 두번째 자리 요소와 자리를 교환한다 그리고 고정된다
+// 3. 이하 반복
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
